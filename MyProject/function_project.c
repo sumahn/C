45e2877779436d7040ba04910de9ebc2032c8434
suman <cktnaks1@gmail.com>
2021-01-10 19:34:23 +0900

    2021-01-10

--- MyProject/function_project.c
+++ MyProject/function_project.c
@@ -7,7 +7,7 @@ void showQuestion(int level, int num1, int num2);
 void success();
 void fail();
 
-int main(void)
+int main_function_project(void)
 {
 	// 문이 5개가 있고, 각 문마다 점점 어려운 수식 퀴즈가 출제된다.(랜덤)
 	// 문제를 맞히면 통과, 틀리면 실패
